37fc192a7fae82a43f901a24e106d5ae5f8caace
BOUKCHOUCH <wafaaboukchouch21@gmail.com>
2026-04-18 22:25:48 +0200

    Update FondsInsuffisantsException.java

--- src/main/java/FondsInsuffisantsException.java
+++ src/main/java/FondsInsuffisantsException.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
-public class FondsInsuffisantsException {
+public class FondsInsuffisantsException extends Exception {
+    private double montantManquant;
+    private double soldeActuel;
+    
+    public FondsInsuffisantsException(String message, double montantManquant, double soldeActuel) {
+        super(message);
+        this.montantManquant = montantManquant;
+        this.soldeActuel = soldeActuel;
+    }
+    
+    public double getMontantManquant() {
+        return montantManquant;
+    }
+    
+    public double getSoldeActuel() {
+        return soldeActuel;
+    }
 }
