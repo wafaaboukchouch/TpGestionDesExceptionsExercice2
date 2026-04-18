4e26335952531b2f4024bf75e0be466690ebcca1
BOUKCHOUCH <wafaaboukchouch21@gmail.com>
2026-04-18 22:23:13 +0200

    Update CompteCourant.java

--- src/main/java/CompteCourant.java
+++ src/main/java/CompteCourant.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
-
-public class CompteCourant {
+public class CompteCourant extends CompteBancaire {
+    private double decouvertAutorise;
+    
+    public CompteCourant(String numeroCompte, String nomTitulaire, double soldeInitial, double decouvertAutorise) {
+        super(numeroCompte, nomTitulaire, soldeInitial);
+        this.decouvertAutorise = decouvertAutorise;
+    }
+    
+    @Override
+    public void retirer(double montant) throws FondsInsuffisantsException {
+        if (montant <= 0) {
+            System.out.println("Erreur : Le montant du retrait doit être positif !");
+            return;
+        }
+        
+        if (solde - montant < -decouvertAutorise) {
+            double montantManquant = Math.abs(solde - montant) - decouvertAutorise;
+            throw new FondsInsuffisantsException(
+                "Fonds insuffisants ! Découvert autorisé de " + decouvertAutorise + " € dépassé.",
+                montantManquant, solde
+            );
+        }
+        
+        solde -= montant;
+        System.out.println("Retrait de " + montant + " € effectué. Nouveau solde : " + solde + " €");
+    }
+    
+    public double getDecouvertAutorise() {
+        return decouvertAutorise;
+    }
 }
