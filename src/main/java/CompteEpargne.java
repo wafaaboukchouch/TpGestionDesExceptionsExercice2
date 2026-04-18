12f9dba08c7028c05317652820dd421cdb180b5d
BOUKCHOUCH <wafaaboukchouch21@gmail.com>
2026-04-18 22:23:47 +0200

    Update CompteEpargne.java

--- src/main/java/CompteEpargne.java
+++ src/main/java/CompteEpargne.java
@@ -1,4 +1,38 @@
-package PACKAGE_NAME;
 
-public class CompteEpargne {
+public class CompteEpargne extends CompteBancaire {
+    private double tauxInteret;
+    
+    public CompteEpargne(String numeroCompte, String nomTitulaire, double soldeInitial, double tauxInteret) {
+        super(numeroCompte, nomTitulaire, soldeInitial);
+        this.tauxInteret = tauxInteret;
+    }
+    
+    @Override
+    public void retirer(double montant) throws FondsInsuffisantsException {
+        if (montant <= 0) {
+            System.out.println("Erreur : Le montant du retrait doit être positif !");
+            return;
+        }
+        
+        if (solde - montant < 0) {
+            throw new FondsInsuffisantsException(
+                "Fonds insuffisants ! Solde actuel : " + solde + " €, retrait demandé : " + montant + " €",
+                montant - solde, solde
+            );
+        }
+        
+        solde -= montant;
+        System.out.println("Retrait de " + montant + " € effectué. Nouveau solde : " + solde + " €");
+    }
+    
+    // Application des intérêts
+    public void appliquerInterets() {
+        double interets = solde * tauxInteret / 100;
+        solde += interets;
+        System.out.println("Intérêts de " + interets + " € appliqués. Nouveau solde : " + solde + " €");
+    }
+    
+    public double getTauxInteret() {
+        return tauxInteret;
+    }
 }
